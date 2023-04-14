c8a5cb295b5a55d3ec8c65b8afebe0b7f02cccd3
zeineb <zaineb.benaziza@esprit.tn>
2023-04-14 06:39:37 +0100

    first-commit

---
+++ src/Entity/SuiviReclamation.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * SuiviReclamation
+ *
+ * @ORM\Table(name="suivi_reclamation", indexes={@ORM\Index(name="CLE_ETRANGERE", columns={"id_reclamation"})})
+ * @ORM\Entity
+ */
+class SuiviReclamation
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="etat_reclamation", type="string", length=255, nullable=true)
+     */
+    private $etatReclamation;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="reponse_reclamation", type="string", length=255, nullable=true)
+     */
+    private $reponseReclamation;
+
+    /**
+     * @var \Reclamation
+     *
+     * @ORM\ManyToOne(targetEntity="Reclamation")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="id_reclamation", referencedColumnName="Id")
+     * })
+     */
+    private $idReclamation;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEtatReclamation(): ?string
+    {
+        return $this->etatReclamation;
+    }
+
+    public function setEtatReclamation(?string $etatReclamation): self
+    {
+        $this->etatReclamation = $etatReclamation;
+
+        return $this;
+    }
+
+    public function getReponseReclamation(): ?string
+    {
+        return $this->reponseReclamation;
+    }
+
+    public function setReponseReclamation(?string $reponseReclamation): self
+    {
+        $this->reponseReclamation = $reponseReclamation;
+
+        return $this;
+    }
+
+    public function getIdReclamation(): ?Reclamation
+    {
+        return $this->idReclamation;
+    }
+
+    public function setIdReclamation(?Reclamation $idReclamation): self
+    {
+        $this->idReclamation = $idReclamation;
+
+        return $this;
+    }
+
+
+}
---
+++ src/Form/SuiviReclamationType.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Form;
+use App\Entity\Reclamation;
+
+use App\Entity\SuiviReclamation;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Choice;
+
+class SuiviReclamationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('etatReclamation', ChoiceType::class, [
+                'choices' => [
+                    'Open' => 'Open',
+                    'In Progress' => 'In Progress',
+                    'Closed' => 'Closed',
+                ],
+                'placeholder' => 'Select a state',
+                'label' => 'State',
+                'required' => true,'label_attr' => [
+                    'style' => 'margin-top: 30px;margin-buttom: 60px; margin-right:40px;'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please select a state',
+                    ]),
+                    new Choice([
+                        'choices' => [
+                            'Open',
+                            'In Progress',
+                            'Closed',
+                        ],
+                        'message' => 'Please select a valid state',
+                    ]),
+                ],
+            ])
+            ->add('reponseReclamation', TextType::class, [
+                'label' => 'Description',
+                'required' => true,
+                'attr' => [
+                    'placeholder' => 'Enter the description here',
+                    'class' => 'form-control',
+                ],
+                'data' => 'Not treated yet','label_attr' => [
+                    'style' => 'margin-top: 30px;margin-buttom: 160px; margin-right:40px;'
+                ], // set default value
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter the description',
+                    ]),
+                ],
+            ])
+            
+            ->add('idReclamation', EntityType::class, [
+                'class' => 'App\Entity\Reclamation',
+                'choice_label' => 'contenu',
+                'placeholder' => 'Select a reclamation',
+                'label' => 'Reclamation',
+                'required' => true,'label_attr' => [
+                    'style' => 'margin-top: 130px; margin-right:30px; position:relative ; bottom:90px;'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please select a reclamation',
+                    ]),
+                ],
+            ])
+            ->add('idReclamation', EntityType::class, [
+                'class' => Reclamation::class,
+                'choice_label' => function (Reclamation $reclamation) {
+                    return $reclamation->getId().' - '.$reclamation->getContenu();
+                },
+                'placeholder' => 'Select a reclamation',
+                'label' => 'Reclamation',
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please select a reclamation',
+                    ]),
+                ],
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Create',
+                'attr' => [
+                    'class' => 'btn btn-primary',
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => SuiviReclamation::class,
+        ]);
+    }
+}
