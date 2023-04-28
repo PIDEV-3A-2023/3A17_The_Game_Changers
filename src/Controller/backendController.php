eff7dd89f3557629510cc008ea840f7e82936742
ademnsir <nsir.adem@esprit.tn>
2023-04-28 04:47:00 +0100

    second

--- src/Controller/backendController.php
+++ src/Controller/backendController.php
@@ -5,18 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\VehiculeType;
+use App\Entity\Vehicule;
+use App\Entity\Reservation;
+use App\Form\ReservationType;
 
-class backendController extends AbstractController
+class BackendController extends AbstractController
 {
-    /**
-     * @Route("/dashboard", name="dashboard")
-     */
+    #[Route('/back', name: 'app_backend')]
     public function index(): Response
     {
-        return $this->render('backend/index.html.twig');
+        return $this->render('backend/index.html.twig', [
+            'controller_name' => 'BackendController',
+        ]);
     }
-
-   
-   
-
 }
